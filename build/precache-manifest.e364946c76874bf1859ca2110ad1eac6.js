5aadc7879e40d52dce4ca1f76a700bd7fc5c2ff2
Josh Pike <9585787+jpike88@users.noreply.github.com>
2023-07-31 16:29:43 +1000

    build commited to repo

---
+++ build/precache-manifest.e364946c76874bf1859ca2110ad1eac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfe6bea18e415ef5c977ffcd7a17dcd5",
+    "url": "/knex-querylab/index.html"
+  },
+  {
+    "revision": "ac353460a13a9bf009c9",
+    "url": "/knex-querylab/static/css/2.da2a1a23.chunk.css"
+  },
+  {
+    "revision": "7943c493050e0a7833c3",
+    "url": "/knex-querylab/static/css/main.05a3ccdb.chunk.css"
+  },
+  {
+    "revision": "ac353460a13a9bf009c9",
+    "url": "/knex-querylab/static/js/2.31ea5493.chunk.js"
+  },
+  {
+    "revision": "7943c493050e0a7833c3",
+    "url": "/knex-querylab/static/js/main.d8b8208d.chunk.js"
+  },
+  {
+    "revision": "d88110396bfaf9589a5a",
+    "url": "/knex-querylab/static/js/runtime~main.0f84ab14.js"
+  }
+]);
